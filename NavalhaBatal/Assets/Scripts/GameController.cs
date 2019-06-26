38a27f217f28f58af2c87167c501f2a4f3af2b1e
willian matheus <willian_matheus14@hotmail.com>
2019-06-26 02:09:59 -0300

    alteracao de gameplay 2.0

--- NavalhaBatal/Assets/Scripts/GameController.cs
+++ NavalhaBatal/Assets/Scripts/GameController.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject[] sea;
+
+    public Sprite SeaSprite;
+    private int BoardSize = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(GameObject o in sea)
+        {
+            o.GetComponent<SpriteRenderer>().sprite = SeaSprite;
+            o.gameObject.name = BoardSize.ToString();
+            BoardSize++;
+        }
     }
 
     // Update is called once per frame
